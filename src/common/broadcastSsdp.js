cbaa61b757218d3082377685b03f96f71ac4bcde
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-19 07:00:17 -0400

    Fix the query, template string, to not included unwanted whitespace.
    Create a udp4 socket using dgram. print the messsage received by that socket, and send the query to the ssdp host:port.

--- src/common/broadcastSsdp.js
+++ src/common/broadcastSsdp.js
@@ -1,14 +1,36 @@
-const ssdpAddress = '239.255.255.250:1900';
+const ssdpAddress = '239.255.255.250';
 const ssdpPort = '1900';
 const searchTarget = 'roku:ecp';
 
-const query = new Buffer(
-    `M-SEARCH * HTTP/1.1\r\n
-    HOST:${ssdpAddress}:${ssdpPort}\r\n
-    MAN:"ssdp:discover"\r\n
-    ST:${searchTarget}\r\n
-    MX:5\r\n
-    \r\n`
-);
+const queryString = `M-SEARCH * HTTP/1.1\r\nHOST: ${ssdpAddress}:${ssdpPort}\r\nMAN: "ssdp:discover"\r\nST: ${searchTarget}\r\nMX: 5\r\n\r\n`;
 
-module.exports = (socket, cb) => socket.send(query, 0, query.length, ssdpPort, ssdpAddress, cb);
+const query = new Buffer(queryString);
+
+const broadcastSsdp = (socket, cb) => socket.send(query, 0, query.length, ssdpPort, ssdpAddress, cb);
+
+const dgram = require('dgram');
+
+const client = dgram.createSocket('udp4');
+
+client.on('error', (err) => {
+	console.log("client had an error", err);
+})
+
+client.on('message', (msg, rinfo) => {
+	console.log(`server received: ${msg}`);
+});
+
+client.on('listening', () => {
+	const address = client.address();
+	console.log(`listening on ${address.address}:${address.port}`);
+});
+
+client.bind(() => {
+	console.log('client port', client.address().port);
+	
+	broadcastSsdp(client, (err) => {
+		if(err) console.log(err);
+		else console.log('broadcasted');
+		//setTimeout(() => { client.close(); }, 2000);
+	});
+});
