a6c9a11c44502c397f199f87988424130f588ea8
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-18 22:03:55 -0400

    api returns the baseUrl to the roku and ignores axios

--- src/common/api.js
+++ src/common/api.js
@@ -1,15 +1,10 @@
-const protocol = 'http';
-const host = '10.0.0.8';
-const port = 8060;
-const baseUrl = `${protocol}://${host}:${port}`;
+const PROTOCOL = 'http';
+const HOST = '10.0.0.8';
+const PORT = 8060;
 const keys = require('./keys');
 
 const axios = require('axios');
 
-const http = axios.create({
-    baseURL: baseUrl,
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-});
+const roku = (host = HOST, port = PORT, protocol = PROTOCOL) => `${protocol}://${host}:${port}`;
+
+export default roku;
