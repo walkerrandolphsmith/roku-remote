a245a579847bac31462da9a60c1d283b36bce65b
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-20 08:42:39 -0400

    Move createStore to the root of common.

--- src/Main.js
+++ src/Main.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import App from './app/components/App';
 
-import createStore from './createStore'
+import createStore from './common/createStore'
 
 const store = createStore();
 
--- src/common/createStore.js
+++ src/common/createStore.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
-import reducer from './common/modules';
+import reducer from './modules';
 
 const middleware = applyMiddleware(thunk);
 
