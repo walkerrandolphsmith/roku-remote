41f1eec9fbf4546284fa1fcff55256c244fdeafb
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-20 09:18:38 -0400

    Rm mock response. Create a __tests__ dir for jest.

--- __tests__/getActiveApp.tests.js
+++ __tests__/getActiveApp.tests.js
@@ -1,4 +1,4 @@
-module.exports = `
+const activeAppResponse = `
 <?xml version="1.0" encoding="UTF-8" ?>
 <active-app>
     <app id="12" type="appl" version="4.2.132">Netflix</app>
--- __tests__/getDetails.tests.js
+++ __tests__/getDetails.tests.js
@@ -1,4 +1,21 @@
-module.exports = `
+const allChannelsResponse = `
+<?xml version="1.0" encoding="UTF-8" ?>
+<apps>
+    <app id="31012" type="menu" version="1.6.8">Movie Store and TV Store</app>
+    <app id="31863" type="menu" version="1.2.6">Roku Home News</app>
+    <app id="12" type="appl" version="4.2.132">Netflix</app>
+    <app id="43594" type="appl" version="1.3.60">Cinema Now</app>
+    <app id="13" type="appl" version="6.0.0">Amazon Video</app>
+    <app id="46041" type="appl" version="5.0.13">Sling TV </app>
+    <app id="8838" type="appl" version="1.6.1944">Showtime</app>
+    <app id="2285" type="appl" version="4.5.3">Hulu</app>
+    <app id="2213" type="appl" version="4.1.1521">Roku Media Player</app>
+    <app id="19977" type="appl" version="1.0.80000021">Spotify</app>
+    <app id="65917" type="appl" version="1.2.0">TheGreatCoursesPlus</app>
+    <app id="81920" type="appl" version="1.0.0">History Vault</app>
+</apps>`;
+
+const deviceInfoResponse = `
 <?xml version="1.0" encoding="UTF-8" ?>
 <device-info>
     <udn>29a40003-6807-10ef-800f-000000000000</udn>
