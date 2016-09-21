7828cbf335363e6779c2d2c9392c00472ff288f6
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-21 02:16:35 -0400

    Fetch icons and render them in all channels

--- src/components/ChannelList/index.styles.js
+++ src/components/ChannelList/index.styles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
 
 export default StyleSheet.create({
     wrapper: {
@@ -6,7 +6,24 @@ export default StyleSheet.create({
         alignSelf: 'stretch'
     },
     channel: {
-        marginLeft: 25,
-        marginTop: 60
+        marginTop: 60,
+        flexDirection: 'row'
+    },
+    labelWrapper: {
+        flex: 3,
+        alignItems: 'center'
+    },
+    label: {
+        width: 300
+    },
+    iconWrapper: {
+        flex: 1,
+        alignItems: 'center',
+        marginLeft: 20
+    },
+    icon: {
+        width: 32,
+        height: 32,
+        resizeMode: Image.resizeMode.contain
     }
 });
--- src/shared/modules/getAppIcons.js
+++ src/shared/modules/getAppIcons.js
@@ -1,4 +1,5 @@
 import { createAction } from 'redux-actions';
+import RNFetchBlob from 'react-native-fetch-blob'
 
 const ACTION = 'SET_APP_ICON';
 
@@ -11,19 +12,21 @@ export const getAppIcons = () => (dispatch, getState) => {
     const channels = getState().atom.channels;
     const url = getState().atom.rokus[0].url;
     channels.forEach(channel => {
-        fetch(`${url}query/icon/${channel.id}`, {
-            method: 'GET'
-        }).then(res => {
-            //react-native-fetch-blob
-            dispatch(success(channel.id, 'icon'));
+        RNFetchBlob.fetch('GET', `${url}query/icon/${channel.id}`).then(res => {
+            const base64Str = res.base64();
+            dispatch(success(channel.id, base64Str));
         });
     })
 };
 
 export const reducer = (state, payload) => {
     const { id, icon } = payload;
-    const channelToUpdate = state.channels.find(channel => channel.id === id);
-    //channelToUpdate.icon = icon;
+    state.channels.forEach(channel => {
+        if(channel.id === id) {
+            channel.icon = icon;
+        }
+    });
+    state.channels = [...state.channels];
     return { ...state };
 };
 
