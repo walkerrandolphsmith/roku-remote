31d6cbb43df831f5901796c600979165028733b5
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-22 20:35:40 -0400

    Change the play/pause icon when it is clicked

--- src/components/Remote/RemoteButtons/PlayButton.js
+++ src/components/Remote/RemoteButtons/PlayButton.js
@@ -3,14 +3,24 @@ import AbstractButton from './AbstractButton';
 import Icon from 'react-native-vector-icons/FontAwesome'; 
 
 export default class PlayButton extends AbstractButton {
+
+    state = {
+        isPaused: false
+    };
+
     childHandler = () => {
         this.handler(this.props.PLAY);
+        this.setState({ isPaused: !this.state.isPaused })
     };
 
     render(){
+
+        const name = this.state.isPaused ? 'pause' : 'play';
+
+
         return (
             <Icon
-                name="play"
+                name={name}
                 size={AbstractButton.defaultProps.size}
                 color={AbstractButton.defaultProps.color}
                 onPress={this.childHandler.bind(this)}
