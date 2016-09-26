a4391d3f060a25abe751af847e4df39503d0c9d5
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-25 23:12:55 -0400

    Don't vertically center the remote screen.

--- src/components/App.styles.js
+++ src/components/App.styles.js
@@ -3,22 +3,19 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     remote: {
         flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#000'
+        justifyContent: 'flex-start',
+        paddingTop: 30,
+        alignItems: 'center'
     },
-
     channels: {
         flex: 1,
         alignItems: 'center',
-        alignSelf: 'stretch',
-        backgroundColor: '#000'
+        alignSelf: 'stretch'
     },
-
     settings: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#000'
+        alignItems: 'center'
     },
     dot: {
         backgroundColor:'rgba(168, 94, 245,0.5)',
