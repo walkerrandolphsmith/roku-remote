e1dbeedba2687e7134dada4066b0468cfb8c8fd9
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-09-30 20:20:56 -0400

    Chai for assertions and sinon for spies.

---
+++ src/parseDeviceInfoResponse.spec.js
@@ -0,0 +1,31 @@
+import { parseDeviceInfoResponse } from './onLoad';
+
+describe('Given xml from querying device info', () => {
+    let xml;
+    let actual;
+
+    beforeEach(() => {
+        xml = `
+<?xml version="1.0" encoding="UTF8" ?>
+<device-info>
+    <udn>29a40003680710ef800f000000000000</udn>
+    <serialnumber>YY003J519951</serialnumber>
+</device-info>
+`;
+    });
+
+    describe('when parsing the device info into json', () => {
+        beforeEach(() => {
+            actual = parseDeviceInfoResponse(xml);
+        });
+
+        it('should create key value pairs for each xml node', () => {
+            const expected = {
+                'udn': '29a40003680710ef800f000000000000',
+                'serialnumber': 'YY003J519951'
+            };
+            expect(actual).to.deep.equal(expected);
+        });
+    });
+});
+
