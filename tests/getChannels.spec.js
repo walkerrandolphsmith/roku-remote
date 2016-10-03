62a3959faa32614daba35a894c63c0fcf6944151
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-03 17:48:10 -0400

    Write specs for getting channels.

--- tests/getChannels.spec.js
+++ tests/getChannels.spec.js
@@ -1,27 +1,40 @@
-import { getChannels } from './../src/onLoad';
+import { __RewireAPI__, getChannels } from './../src/onLoad';
 import fetchMock from 'fetch-mock';
 
-describe('src/onLoad/getChannels', () => {
-    // describe('Given the root url', () => {
-    //     let rootUrl, urlPattern;
-    //     beforeEach(() => {
-    //         rootUrl = 'rootUrl/';
-    //         urlPattern = /rootUrl\/query\/apps/;
-    //     });
-    //
-    //     afterEach(() => {
-    //         fetchMock.restore();
-    //     });
-    //
-    //     describe('When querying for channel info', () => {
-    //         beforeEach(() => {
-    //             fetchMock.mock(urlPattern, 200);
-    //             getChannels(rootUrl);
-    //         });
-    //
-    //         it('Then fetch should be called with rootUrl/query/apps as the parameter', () => {
-    //             expect(fetchMock.called(urlPattern).to.equal(true));
-    //         });
-    //     });
-    // });
+describe('given a url', () => {
+    let url, uri, xml;
+    beforeEach(() => {
+        url = 'URL';
+        uri = 'URI';
+        xml = `
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <apps>
+            <app id="31012" type="menu" version="1.6.8">Movie Store and TV Store</app>
+        </apps>`;
+        __RewireAPI__.__Rewire__('RNFetchBlob', {
+            fetch: () => Promise.resolve({ base64: () => uri })
+        });
+        fetchMock.get('*', xml);
+    });
+
+    afterEach(() => {
+        fetchMock.restore();
+    });
+
+    describe('when getting channelz', () => {
+        let actual, expected;
+        beforeEach((done) => {
+            expected = [
+                { id: '31012', type: 'menu', version: '1.6.8', name: 'Movie Store and TV Store', icon: uri }
+            ];
+            getChannels(url).then(res => {
+                actual = res;
+                done();
+            });
+        });
+
+        it('it get the apps at the url, get their icons, and then merge the sets', () => {
+            expect(actual).to.deep.equal(expected);
+        });
+    });
 });
