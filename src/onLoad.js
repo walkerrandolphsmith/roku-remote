7a4227db414219c51d25ba9b6999be87db39be0f
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2016-10-12 23:33:44 -0400

    Immediately JSON prase the retreived data from storage.

--- src/onLoad.js
+++ src/onLoad.js
@@ -19,18 +19,14 @@ const findRokus = async () => {
 
 const getData = async () => {
     try {
-        let storedData = await AsyncStorage.getItem(STORAGE_KEY);
-        storedData = JSON.parse(storedData);
-        if(storedData) {
-            const deviceInfo = await getDeviceInfo(storedData.device.url);
-            if(deviceInfo) {
-                return { fromStorage: true, ...storedData };
-            } else {
-                return await findRokus();
-            }
-        } else {
+        const storedData = JSON.parse(
+            await AsyncStorage.getItem(STORAGE_KEY)
+        );
+        if(!storedData) {
             return await findRokus();
         }
+        const deviceInfo = await getDeviceInfo(storedData.device.url);
+        return await deviceInfo ? { fromStorage: true, ...storedData } : findRokus();
     } catch (error) {
         return new Error('An error occured remembering your preferences.');
     }
